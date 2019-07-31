3d9f3a038e18b9cc1a942dfe4346cc97c57376b7
Samantha V <sdvillafloran@gmail.com>
2019-07-30 23:53:01 -0700

    Update Stop_Bullying.js

--- Stop_Bullying.js
+++ Stop_Bullying.js
@@ -82,3 +82,15 @@ function Popup() {
   var popup = document.getElementById("myPopup");
   popup.classList.toggle("show");
 }
+
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > -3 || document.documentElement.scrollTop > -3) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+}
