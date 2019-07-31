3c9cc0f30c74770dc40834065758f60cb59a7f06
Samantha V <sdvillafloran@gmail.com>
2019-07-31 00:09:42 -0700

    Update Navigation.js

--- Navigation.js
+++ Navigation.js
@@ -2,7 +2,7 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > -3 || document.documentElement.scrollTop > -3) {
-    document.getElementById("navbar").style.top = "0";
+    document.getElementById("navbar").style.top = "3px";
   } else {
     document.getElementById("navbar").style.top = "-50px";
   }
